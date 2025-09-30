3afe53937812a43c60bcadd7795bb53cf9d20de8
StieltjesDev <stieltjesdev@gmail.com>
2025-09-30 12:38:05 -0300

    Alteração no arquivos routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import users from './users.js';
-import decks from './deck.js';
+import decks from './decks.js';
+import events from './events.js';
 
 const router = Router();
 router.use('/decks', decks);
 router.use('/users', users);
+router.use('/events', event);
 
 export default router;
